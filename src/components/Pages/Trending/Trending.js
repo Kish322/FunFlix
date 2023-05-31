ee6790430c51d2f97146a605fc217e9fa0dda57f
Kish322 <scarecrowkishor81@gmail.com>
2023-05-30 20:23:33 -0400

    create page bar

--- src/components/Pages/Trending/Trending.js
+++ src/components/Pages/Trending/Trending.js
@@ -2,24 +2,26 @@ import axios from 'axios';
 import './Trending.css';
 import { useEffect, useState } from 'react';
 import SingleContent from '../../SingleContent/SingleContent';
+import CustomPagination from '../../Header/Pagination/CustomPagination';
 
 const Trending = () => {
+  const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
 
-  const fetchTrending = async () => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
-    );
-    setContent(data.results);
-  };
-
   useEffect(() => {
+    const fetchTrending = async () => {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+      );
+      setContent(data.results);
+    };
+
     fetchTrending();
-  }, []);
+  }, [page]);
 
   return (
     <div className="trending-container">
-      <span className="pageTitle"></span>
+      <h1 className="trending-heading"> Trending </h1>
       <div className="trending">
         {content &&
           content.map((c) => (
@@ -31,11 +33,15 @@ const Trending = () => {
               date={c.first_air_date || c.release_date}
               media_type={c.media_type}
               vote_average={c.vote_average}
+              origin_country={c.origin_country}
+              original_language={c.original_language}
             />
           ))}
       </div>
+      <CustomPagination setPage={setPage} />
     </div>
   );
 };
 
 export default Trending;
+
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,3 +1,4 @@
+import { Badge } from "@material-ui/core";
 import { img_300, unavailable } from "../config/config";
 import './SingleContent.css';
 
@@ -8,22 +9,35 @@ const SingleContent = ({
   date,
   media_type,
   vote_average,
+  origin_country,
+  original_language
 }) => {
   const mediaTypeText = media_type === 'tv' ? "TV Series" : "Movie";
-
+  const ratingText = `Rating:\n${vote_average}`;
+  
   return (
     <div className="media">
+      <Badge className="badge" badgeContent={ratingText} color={vote_average > 6 ? 'primary' : 'secondary' } />
       <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
       <div className="contentDetails">
         <b className="title">{title}</b>
         <div className="subTitle">
-          <span className="mediaType">{mediaTypeText}</span>
-          <span className="date">{date}</span>
+          <span className="mediaType">Type: {mediaTypeText}</span>
+          <span className="date">Released Date: {date}</span>
+          {origin_country ? (
+            <span className="originCountry">Origin Country: {origin_country}</span>
+          ) : (
+            <span className="originCountry">Origin Country: N/A</span>
+          )}
+          <span className="originalLanguage">Original Language: {original_language}</span>
         </div>
       </div>
     </div>
   );
 };
 
-export default SingleContent;
+export default SingleContent
+
+
+
 
