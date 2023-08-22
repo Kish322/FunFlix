28ff79f962a7d42338b8b7d5be2dfee00871fad2
Kishani <kishani@Kishanis-MacBook-Pro.local>
2023-08-22 13:05:53 +0530

    fix type tvSeries issue

--- src/Pages/GenreChart/GenreChart.js
+++ src/Pages/GenreChart/GenreChart.js
@@ -6,98 +6,74 @@ import Chart from 'chart.js/auto';
 import { CategoryScale } from 'chart.js';
 import './GenreChart.css';
 Chart.register(CategoryScale);
-
 const GenreChart = () => {
   const [genreData, setGenreData] = useState(null);
   const [selectedType, setSelectedType] = useState('movie'); // Default to 'movie'
   const [selectedYear, setSelectedYear] = useState('');
-  const [selectedCountry, setSelectedCountry] = useState('');
   const [selectedLanguage, setSelectedLanguage] = useState('');
+  const [languageList, setLanguageList] = useState([]);  
   const [selectedRating, setSelectedRating] = useState('');
   const [selectedGenre, setSelectedGenre] = useState('');
   const [movieNames, setMovieNames] = useState([]);
+  const [tvSeriesNames, setTvSeriesNames] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const chartRef = useRef(null);
-
-  const years = Array.from({ length: new Date().getFullYear() - 1990 + 1 }, (_, index) => 1990 + index);
-  const countries = ['US', 'GB', 'CA', 'AU', 'DE', 'JP'];
-  const languages = ['en', 'es', 'fr', 'de', 'it', 'ja', 'ko', 'ar', 'zh'];
-  const ratings = Array.from({ length: 10 }, (_, index) => index + 1);
-  const countryNames = {
-    US: 'United States',
-    GB: 'United Kingdom',
-    CA: 'Canada',
-    AU: 'Australia',
-    DE: 'Germany',
-    JP: 'Japan',
-  };
-  const languageNames = {
-    en: 'English',
-    es: 'Spanish',
-    fr: 'French',
-    de: 'German',
-    it: 'Italian',
-    ja: 'Japanese',
-    zh: 'Chinese',
-    ko: 'Korean',
-    ar: 'Arabic',
-  };
-
+  const years = Array.from({ length: new Date().getFullYear() - 1990 + 1 }, (_, index) => 1990 + index); 
+  const ratings = Array.from({ length: 10 }, (_, index) => index + 1); // Rating values from 1 to 10
+  
   const tmdbLogoUrl =
     'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg';
   const tmdbApiUrl = 'https://www.themoviedb.org/';
-
   useEffect(() => {
     const fetchGenreData = async () => {
       try {
         setIsLoading(true);
-
         const response = await axios.get(
           `https://api.themoviedb.org/3/genre/${selectedType}/list?api_key=${process.env.REACT_APP_API_KEY}`
         );
         const genres = response.data.genres;
-
         const genreCounts = {};
-        genres.forEach((genre) => {
+        genres.forEach(genre => {
           genreCounts[genre.name] = 0;
         });
-
         let apiUrl = `https://api.themoviedb.org/3/discover/${selectedType}?api_key=${process.env.REACT_APP_API_KEY}&sort_by=popularity.desc&include_adult=false&include_video=false&page=${currentPage}`;
-
-        if (selectedYear) {
-          apiUrl += `&primary_release_year=${selectedYear}`;
-        }
-        if (selectedCountry) {
-          apiUrl += `&region=${selectedCountry}`;
-        }
         if (selectedLanguage) {
           apiUrl += `&with_original_language=${selectedLanguage}`;
         }
         if (selectedRating) {
           apiUrl += `&vote_average.gte=${selectedRating}&vote_average.lte=${selectedRating}`;
         }
-
-        const popularItemsResponse = await axios.get(apiUrl);
-        const popularItems = popularItemsResponse.data.results;
-
-        popularItems.forEach((item) => {
-          item.genre_ids.forEach((genreId) => {
-            const genre = genres.find((g) => g.id === genreId);
+        if (selectedType === 'tv' && selectedYear) {
+          apiUrl += `&first_air_date_year=${selectedYear}`;
+        } 
+        else if (selectedYear) {
+          apiUrl += `&primary_release_year=${selectedYear}`;
+        }
+        const popularMoviesResponse = await axios.get(apiUrl);
+        const popularMovies = popularMoviesResponse.data.results;
+        popularMovies.forEach(movie => {
+          movie.genre_ids.forEach(genreId => {
+            const genre = genres.find(g => g.id === genreId);
             if (genre) {
               genreCounts[genre.name]++;
               if (selectedGenre === genre.name) {
-                setMovieNames((prevNames) => [...prevNames, item.title || item.name]);
+                if (selectedType === 'movie') {
+                  setMovieNames(prevNames => [...prevNames, movie.title]);
+                } else {
+                  // Check for duplicates before adding to tvSeriesNames
+                  if (!tvSeriesNames.includes(movie.name)) {
+                    setTvSeriesNames(prevNames => [...prevNames, movie.name]);
+                  }
+                }
               }
             }
           });
         });
-
         const genreLabels = Object.keys(genreCounts);
         const genreCountValues = Object.values(genreCounts);
-
+        // Define an array of background colors for the bars
         const backgroundColors = ['orange', 'red', 'blue', 'green'];
-
         if (currentPage === 1) {
           setGenreData({
             labels: genreLabels,
@@ -105,14 +81,14 @@ const GenreChart = () => {
               {
                 label: 'Genre Count',
                 data: genreCountValues,
-                backgroundColor: backgroundColors,
+                backgroundColor: backgroundColors, 
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1,
               },
             ],
           });
         } else {
-          setGenreData((prevData) => ({
+          setGenreData(prevData => ({
             ...prevData,
             datasets: [
               {
@@ -122,65 +98,49 @@ const GenreChart = () => {
             ],
           }));
         }
-
         setIsLoading(false);
       } catch (error) {
         console.error(error);
         setIsLoading(false);
       }
     };
-
     fetchGenreData();
-  }, [selectedType, selectedYear, selectedCountry, selectedLanguage, selectedRating, currentPage, selectedGenre]);
-
-  const handleTypeChange = (event) => {
+  }, [selectedType, selectedYear, selectedLanguage, selectedRating, currentPage, selectedGenre, tvSeriesNames]);
+  const handleTypeChange = event => {
     setSelectedType(event.target.value);
     setCurrentPage(1);
     setSelectedGenre('');
     setMovieNames([]);
   };
-
-  const handleYearChange = (event) => {
+  const handleYearChange = event => {
     setSelectedYear(event.target.value);
     setCurrentPage(1);
   };
-
-  const handleCountryChange = (event) => {
-    setSelectedCountry(event.target.value);
-    setCurrentPage(1);
-  };
-
-  const handleLanguageChange = (event) => {
+  const handleLanguageChange = event => {
     setSelectedLanguage(event.target.value);
     setCurrentPage(1);
   };
-
-  const handleRatingChange = (event) => {
+  const handleRatingChange = event => {
     setSelectedRating(event.target.value);
     setCurrentPage(1);
   };
-
   const handleBoxClick = () => {
     window.location.href = tmdbApiUrl;
   };
-
   const handleScroll = () => {
     if (
       chartRef.current &&
       window.innerHeight + window.pageYOffset >= chartRef.current.offsetTop + chartRef.current.offsetHeight
     ) {
-      setCurrentPage((prevPage) => prevPage + 1);
+      setCurrentPage(prevPage => prevPage + 1);
     }
   };
-
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
-
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-
   const handleBarClick = (_, elements) => {
     if (elements.length > 0) {
       const clickedIndex = elements[0].index;
@@ -188,13 +148,29 @@ const GenreChart = () => {
       if (selectedGenre === clickedGenre) {
         setSelectedGenre('');
         setMovieNames([]);
+        setTvSeriesNames([]); // Clear TV series names as well
       } else {
         setSelectedGenre(clickedGenre);
         setMovieNames([]);
+        setTvSeriesNames([]); // Clear TV series names as well
       }
     }
   };
 
+  useEffect(() => {
+    const fetchLanguages = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/configuration/languages?api_key=${process.env.REACT_APP_API_KEY}`
+        );
+        setLanguageList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchLanguages();
+  }, []);
+  
   return (
     <div className="container">
       <div className="genre-title">Popular Genre Distribution</div>
@@ -202,7 +178,7 @@ const GenreChart = () => {
         <Typography variant="subtitle1" component="label" htmlFor="type-select">
           Select Type:
         </Typography>
-        <select id="type-select" value={selectedType} onChange={handleTypeChange}>
+        <select id="type-select" value={selectedType} onChange={handleTypeChange} style={{ textAlign: 'center' }}>
           <option value="movie">Movies</option>
           <option value="tv">TV Series</option>
         </select>
@@ -211,24 +187,11 @@ const GenreChart = () => {
         <Typography variant="subtitle1" component="label" htmlFor="year-select">
           Select Year:
         </Typography>
-        <select id="year-select" value={selectedYear} onChange={handleYearChange}>
+        <select id="year-select" value={selectedYear} onChange={handleYearChange} style={{ textAlign: 'center' }}>
           <option value="">All</option>
-          {years.map((year) => (
+          {years.map(year => (
             <option key={year} value={year}>
               {year}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="select-container country-box">
-        <Typography variant="subtitle1" component="label" htmlFor="country-select">
-          Select Released Country:
-        </Typography>
-        <select id="country-select" value={selectedCountry} onChange={handleCountryChange}>
-          <option value="">All</option>
-          {countries.map((country) => (
-            <option key={country} value={country}>
-              {countryNames[country]}
             </option>
           ))}
         </select>
@@ -237,22 +200,22 @@ const GenreChart = () => {
         <Typography variant="subtitle1" component="label" htmlFor="language-select">
           Select Language:
         </Typography>
-        <select id="language-select" value={selectedLanguage} onChange={handleLanguageChange}>
-          <option value="">All</option>
-          {languages.map((language) => (
-            <option key={language} value={language}>
-              {languageNames[language]}
+        <select id="language-select" value={selectedLanguage} onChange={handleLanguageChange} style={{ textAlign: 'center', width: '19%', height: '40px' }}>
+          <option value="en">English</option>
+            {languageList.map(language => (
+            <option key={language.iso_639_1} value={language.iso_639_1}>
+            {language.english_name}
             </option>
-          ))}
+        ))}
         </select>
       </div>
       <div className="select-container rating-box">
         <Typography variant="subtitle1" component="label" htmlFor="rating-select">
           Select Rating:
         </Typography>
-        <select id="rating-select" value={selectedRating} onChange={handleRatingChange}>
+        <select id="rating-select" value={selectedRating} onChange={handleRatingChange} style={{ textAlign: 'center' }}>
           <option value="">All</option>
-          {ratings.map((rating) => (
+          {ratings.map(rating => (
             <option key={rating} value={rating}>
               {rating}
             </option>
@@ -262,89 +225,91 @@ const GenreChart = () => {
           <p>Content sourced from TMDB</p>
           <img src={tmdbLogoUrl} alt="TMDB Logo" />
         </div>
+        <div className="designed-by">
+          Designed by Kishor Yogeswaran 
+        </div>
       </div>
       {genreData ? (
         <div className="chart-container">
-          <Bar
-            data={genreData}
-            options={{
-              responsive: true,
-              plugins: {
-                legend: {
-                  display: false,
-                },
+           <Bar
+      data={genreData}
+      options={{
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false,
+            },
+            ticks: {
+              color: 'black',
+              font: {
+                size: 15,
               },
-              scales: {
-                x: {
-                  grid: {
-                    display: false,
-                  },
-                  ticks: {
-                    color: 'black',
-                    font: {
-                      size: 15,
-                    },
-                  },
-                  title: {
-                    display: true,
-                    text: 'Genre',
-                    color: 'black',
-                    font: {
-                      size: 18,
-                    },
-                    padding: {
-                      bottom: 10,
-                    },
-                  },
-                },
-                y: {
-                  beginAtZero: true,
-                  grid: {
-                    borderDash: [2],
-                    color: '#ddd',
-                  },
-                  ticks: {
-                    color: 'black',
-                    font: {
-                      size: 15,
-                    },
-                  },
-                  title: {
-                    display: true,
-                    text: 'Count',
-                    color: 'black',
-                    font: {
-                      size: 18,
-                    },
-                    padding: {
-                      top: 10,
-                    },
-                  },
-                },
+            },
+            title: {
+              display: true,
+              text: `Genre Distribution (${selectedType === 'movie' ? 'Movies' : 'TV Series'})`,
+              color: 'black',
+              font: {
+                size: 18,
               },
-              onClick: handleBarClick,
-            }}
-            ref={chartRef}
-          />
-          {selectedGenre && movieNames.length > 0 && (
-            <div className="genre-info">
-              <Typography variant="h4" className="genre-info-title">
-                {selectedGenre}:
-              </Typography>
-              <ul className="genre-info-list">
-                {movieNames.map((movieName) => (
-                  <li key={movieName}>{movieName}</li>
-                ))}
-              </ul>
+              padding: {
+                bottom: 10,
+              },
+            },
+          },
+          y: {
+            beginAtZero: true,
+            grid: {
+              borderDash: [2],
+              color: '#ddd',
+            },
+            ticks: {
+              color: 'black',
+              font: {
+                size: 15,
+              },
+            },
+            title: {
+              display: true,
+              text: 'Count', 
+              color: 'black',
+              font: {
+                size: 18,
+              },
+              padding: {
+                top: 10,
+              },
+            },
+          },
+        },
+        onClick: handleBarClick,
+      }}
+      ref={chartRef}
+    />
+      {selectedGenre && (movieNames.length > 0 || tvSeriesNames.length > 0) && (
+        <div className="genre-info">
+          <Typography variant="h4" className="genre-info-title">
+            {selectedGenre}:
+          </Typography>
+          <ul className="genre-info-list">
+            {selectedType === 'movie'
+              ? movieNames.map(movieName => <li key={movieName}>{movieName}</li>)
+              : tvSeriesNames.map(tvSeriesName => <li key={tvSeriesName}>{tvSeriesName}</li>)}
+          </ul>
             </div>
           )}
         </div>
       ) : (
-        <p></p>
+        <p></p> 
       )}
-      {isLoading && <p></p>}
+      {isLoading}
     </div>
   );
 };
-
 export default GenreChart;
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -15,7 +15,7 @@ const useStyles = makeStyles(() => ({
     left: "50%",
     transform: "translateX(-50%)",
     fontSize: "1rem",
-    color: "black",
+    color: "white",
   },
 }));
 
